a94cdc0ffe3fc57d7d3d4f2ef947547495bc9baa
Widiah Fonika Sari <99710747+widiahfonikasari@users.noreply.github.com>
2022-03-06 19:36:35 +0700

    Update double link list.cpp

--- double link list.cpp
+++ double link list.cpp
@@ -23,8 +23,28 @@ void createDoubleLinkedList(string judul, string pengarang, int tahun_t){
 	tail = head;
 }
 
+// count Double Linked List
+int countDoubleLinkedList()
+{
+  if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+  }else{
+    cur = head;
+    int jumlah = 0;
+    while( cur != NULL ){
+      jumlah++;      
+      //step
+      cur = cur->next;
+    }
+    return jumlah;
+  }
+}
+
 //insert first double linked list
 void insertFirst(string judul, string pengarang, int tahun_t){
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+  	}else{
 	newNode = new Buku();
 	newNode->judul = judul;
 	newNode->pengarang = pengarang;
@@ -33,10 +53,14 @@ void insertFirst(string judul, string pengarang, int tahun_t){
 	newNode->next = head;
 	head->prev = newNode;
 	head = newNode;
+	}
 }
 
 //insert last double linked list
 void insertLast(string judul, string pengarang, int tahun_t){
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+  	}else{
 	newNode = new Buku();
 	newNode->judul = judul;
 	newNode->pengarang = pengarang;
@@ -45,10 +69,14 @@ void insertLast(string judul, string pengarang, int tahun_t){
 	newNode->next = NULL;
 	tail->next = newNode;
 	tail = newNode;
+	}
 }
 
 //insert after double linked list
 void insertAfter(string judul, string pengarang, int tahun_t, int posisi){
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+ 	 }else{
 	newNode = new Buku();
 	newNode->judul = judul;
 	newNode->pengarang = pengarang;
@@ -67,62 +95,84 @@ void insertAfter(string judul, string pengarang, int tahun_t, int posisi){
 	newNode->next - afterNode;
 	cur->next =newNode;
 	afterNode->prev = newNode;
+	}
 }
 
 //delete first double linked list
 void deleteFirst(){
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+	  }else{
 	del = head;
 	head = head->next;
 	head->prev = NULL;
 	delete del;
+	}
 }
 
 //delete last double linked list
 void deleteLast(){
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+  	}else{
 	del = tail;
 	tail = tail->prev;
 	tail->next =NULL;
 	delete del;
+	}
 }
 
 //delete after double linked list
 void deleteAfter(int posisi){
-	int nomor = 1;
-	cur = head;
-	while(nomor <= posisi -1){
-		cur = cur->next;
-		nomor++;
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+ 	 }else{
+ 	 	if( posisi == 1 || posisi == countDoubleLinkedList() ){
+      		cout << "Posisi bukan posisi tegah!" << endl;
+   		}else if( posisi < 1 || posisi > countDoubleLinkedList() ){
+      		cout << "Posisi diluar jangkauan!" << endl;
+   		}else{
+			int nomor = 1;
+			cur = head;
+			while(nomor <= posisi -1){
+				cur = cur->next;
+				nomor++;
+				}
+			del = cur->next;
+			afterNode = del->next;
+			cur->next =afterNode;
+			afterNode->prev = cur;
+			delete del;
+		}
 	}
-	del = cur->next;
-	afterNode = del->next;
-	cur->next =afterNode;
-	afterNode->prev = cur;
-	delete del;
-}
-
-bool searchNode(string judul, string pengarang, int tahun_t, int posisi){
-    newNode = new Buku();
-	newNode->judul = judul;
-	newNode->pengarang = pengarang;
-	newNode->tahun_terbit = tahun_t;
-    while (cur != NULL){
-        if (cur->tahun_terbit == posisi)
-            return true;
-        cur = cur->next;
-    }
-    return false;
 }
 
 //print double linked list
 void printDoubleLinkedList(){
-		cur = head;
-		while(cur != NULL){
-		cout<<"Judul Buku : "<<cur->judul<<endl;
-		cout<<"Pengarang Buku : "<<cur->pengarang<<endl;
-		cout<<"Tahun Terbit Buku : "<<cur->tahun_terbit<<endl;
-		
-		cur = cur->next;
-		}
+	if( head == NULL ){
+    cout << "Double Linked List belum dibuat!";
+  	}else{
+    cout << "\nJumlah Data : " << countDoubleLinkedList() << endl;
+    cout << "INFO DATA" << endl;
+    cur = head;
+    while( cur != NULL ){
+     
+      cout << "Data : " << cur->tahun_terbit << endl;
+      
+      cur = cur->next;
+    }
+  }
+}
+
+void searchNode(Buku **head, int tahun_t ){
+    int currentTT = 2013;
+    Buku*cur = *head;
+    while (cur->next != 0 && currentTT != tahun_t)
+    {
+        cur = cur->next;
+        currentTT++;
+    }
+    cout << cur->judul << " ditemukan pada tahun ke " << currentTT;
 }
 
 int main(){
@@ -159,15 +209,7 @@ int main(){
 	printDoubleLinkedList();
 	cout<<"\n\n"<<endl;
 	
-	searchNode("Kebumian", "Ratna Harto", 2017, 3);
 	printDoubleLinkedList();
-	int tahun_terbit= 2013;
-	
-    if (tahun_terbit >= 2013 && tahun_terbit >2018){
-        cout<<"\nPosisi "<<tahun_terbit<<" Data Tidak Ditemukan";
-    }else{
-        cout<<"\nPosisi "<<tahun_terbit<<" Data Ditemukan";
-	}
-	
+  	searchNode(&head, 3);
 	return 0;
 }
